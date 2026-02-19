d9549ef4713bfc7e8e59519e6519dea4964b7ae6
udaykiran160 <udaykirannaidulingutla@gmail.com>
2026-02-19 13:01:36 +0530

    read

--- specialvar.sh
+++ specialvar.sh
@@ -4,9 +4,9 @@
 echo "all variables pass to the script: $@"
 echo "all variables pass to the script: $*"
 echo "Script name : $0"
-echo "present working directory : $pwd"
-echo "who is running : $user"
-echo "home directory of user : $home"
+echo "present working directory : $PWD"
+echo "who is running : $USER"
+echo "home directory of user : $HOME"
 echo "PID of the script : $$"
 sleep 5 &
 echo "PID of the last command in the background :$!"
