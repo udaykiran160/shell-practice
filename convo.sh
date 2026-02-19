be14dd030c64d76b211eda8269a69057e90734d6
udaykiran160 <udaykirannaidulingutla@gmail.com>
2026-02-19 12:15:32 +0530

    kj

--- convo.sh
+++ convo.sh
@@ -3,4 +3,4 @@ person1=uday
 person2=kiran
 
 echo "$person1::Hi $person2 how aare you?"
-echo "$person2:: hello $person1 Iam fine what about you.
+echo "$person2:: hello $person1 Iam fine what about you."
