65a4e7dd82e7fb12c55bf4e7d951dbb9aa06443f
udaykiran160 <udaykirannaidulingutla@gmail.com>
2026-02-25 17:31:25 +0530

    conditions

--- 12.installation.sh
+++ 12.installation.sh
@@ -4,4 +4,14 @@ USER_ID=$(id -u)
 
 if [ $USER_ID -ne 0 ]; then
     echo "Error:: Please run the script with the root previlage"
+    exit 1 
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0]; then 
+    echo "Error:: installing mysql is a failure"
+    exit 1
+else 
+    echo " installing mysql is success"
 fi
