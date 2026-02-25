7a890285f527d27ed8bb467aa90bdd54508d9628
udaykiran160 <udaykirannaidulingutla@gmail.com>
2026-02-25 17:23:01 +0530

    conditions

--- 12.installation.sh
+++ 12.installation.sh
@@ -3,5 +3,5 @@
 USER_ID=$(id -u)
 
 if [ $USER_ID -ne 0 ]; then
-    else "Error:: Please run the script with the root previlage"
+    echo "Error:: Please run the script with the root previlage"
 fi
